3334faa8c3c3ec8ac5cd22be0457c47d4c3bef05
sjackson44 <sjackson44@gmail.com>
2015-03-14 11:47:38 -0400

    added gems to gem file and fixed the view also added mechanize

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,10 +1,13 @@
 class LocationsController < ApplicationController
 	def index
+
 		api_key = 'giHyMhPCwiaGw3mZRzbky7tlYx7MySHPaonXGcAdvLT2Of2GOd0nkuJj2Q5AJMqU'
 		request = HTTParty.get("https://www.zipcodeapi.com/rest/"+api_key+"/info.json/#{params[:zipcode]}/miles")
-		puts "#{request["city"]}, #{request["state"]}"
-		# send = HTTParty.get("https://127.0.0.1:8080", 
-		# 				body: {city: request["city"], state: request["state"]}
-
+		agent = Mechanize.new
+		agent.get("http://livingwage.mit.edu/")
+		key = agent.page.forms[0]
+		key["q"]="#{request["city"]}, #{request["state"]}"
+		key.submit
+		binding.pry
 	end
 end
